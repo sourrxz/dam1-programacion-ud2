0c068b5598ddc2038b0c9867eec2ed2a558c3f39
sourrxz <sourrxz@gmail.com>
2025-10-01 13:54:09 +0200

    DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -15,17 +15,27 @@ public class DatosUsuario {
         System.out.println("Introduce tu correo electronico: ");
         String correo = sc.nextLine();
         boolean emailValido = ProcesadorTexto.esEmailValido(correo);
-        if (edad>18) {
-            System.out.println("Tienes edad para conducir");
-        } else{
-            System.out.println("No tienes edad para conducir");
-        }
+
+        String nombreNormalizado = String.valueOf(nombre.charAt(0)).toUpperCase() + (nombre.substring(1)).toLowerCase();
+
         if (emailValido) {
-            Usuario usuario = new Usuario(nombre, edad, correo);
+            Usuario usuario = new Usuario(nombreNormalizado, edad, correo);
             usuario.mostrarInformacion();
-        }
-        else{
+            if (edad > 18) {
+                System.out.println("Tienes edad para conducir");
+            } else {
+                System.out.println("No tienes edad para conducir");
+            }
+        } else {
             System.out.println("El usuario no puede ser creado porque el email introducido no es válido");
+
+        }
+        if (edad < 0 && edad > 120) {
+            System.out.println("error");
+        }
+        int NOMBRE_LONGITUD = nombre.length();
+        if (NOMBRE_LONGITUD < 2 || NOMBRE_LONGITUD > 20) {
+            System.out.println("error");
         }
         sc.close();
     }
